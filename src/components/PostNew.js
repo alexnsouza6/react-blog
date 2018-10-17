8fedf27ff4e02347965d72a76f2f89ac15d5b75c
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-16 22:57:30 -0300

    feat: Adding submit and error handlers

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -1,11 +1,82 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostNew extends Component {
+
+    renderField = (field) => {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${ touched && error ? 'has-danger' : '' }`;
+        return(
+            <div className={className}>
+                <label>{ field.label }</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    { ...field.input }
+                />
+                <div className="text-help">
+                    { touched ? error : '' }
+                </div>
+            </div>
+        );
+    }
+
+    onSubmit = (values) => {
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
+    }
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <h3> This is a new PostNew </h3>
+            <form onSubmit={ handleSubmit(this.onSubmit) }>
+                <Field //Abstract data keeper
+                    label="Title"
+                    name="title"
+                    component={ this.renderField }
+                />
+                <Field //Abstract data keeper
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field //Abstract data keeper
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary"> Submit </button>
+                <Link to="/" className="btn btn-danger"> Cancel </Link>
+            </form>
         );
     }
 }
 
-export default PostNew;
+const validate = (values) => {
+    const errors = {};
+
+    if(!values.title){
+        errors.title = "Enter a title!";
+    }
+
+    if(!values.categories){
+        errors.categories = "Enter some categories!";
+    }
+
+    if(!values.content){
+        errors.content = "Enter some content!";
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm' //This has to be a unique string
+})(
+    connect(null,{createPost})(PostNew)
+);
