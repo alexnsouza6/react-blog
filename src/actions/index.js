aefaa75377a9ae05d9f5831c763dc902f252b7c7
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-16 22:56:53 -0300

    feat: Adding createpost and fetchpost action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,31 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'fetch_post';
+export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=DORAVENTUREIRA123';
 
 export const fetchPosts = () => {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export const createPost = (values,callback) => {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+    return {
+        type: CREATE_POST,
+        payload: request
+    }
+}
+
+export const fetchPost = (id) => {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
     return {
         type: FETCH_POSTS,
         payload: request
